ce689ef4b03dcae7a9e0faaa679c14b3fd995419
Sơn Phạm <anson@AnSons-MacBook-Pro.local>
2017-04-09 22:16:22 +0700

    Bài tập 02 - import thông tin vào DataBase

---
+++ 1542266_CowImport/1542266_CowImport/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+
+
+namespace _1542266_CowImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pkgLocation = @"D:\1542266_GetName\1542266_GetName\Package.dtsx";
+
+            Package pkg;
+            Microsoft.SqlServer.Dts.Runtime.Application app;
+            DTSExecResult pkgResults;
+            Variables vars;
+
+            app = new Microsoft.SqlServer.Dts.Runtime.Application();
+            pkg = app.LoadPackage(pkgLocation, null);
+
+            vars = pkg.Variables;
+            vars["User::import_cow"].Value = @"C:\Users\ng.phuocloc\Downloads\chip2.txt";
+
+            pkgResults = pkg.Execute(null, vars, null, null, null);
+
+            if (pkgResults == DTSExecResult.Success)
+                Console.WriteLine("Import successfully");
+            else
+                Console.WriteLine("Import failed");
+        }
+    }
+}
